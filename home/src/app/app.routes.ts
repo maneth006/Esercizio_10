e32c7b5ed4106aa3c63bea79653d0be206e53078
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-04-23 12:00:30 +0000

    commit2

--- home/src/app/app.routes.ts
+++ home/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DescrizioneComponent } from './descrizione/descrizione.component';
+import { HomeComponent } from './home/home.component';
+import { MostreComponent } from './mostre/mostre.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', redirectTo: '/Home', pathMatch: 'full'},
+    
+    {path: 'descrizione', component: DescrizioneComponent}, 
+    
+    {path: 'mostre/:id', component: MostreComponent} ,
+    
+    {path: 'Home', component:HomeComponent},
+];

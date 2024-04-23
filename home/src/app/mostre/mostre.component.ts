81c4031517a14e504071c4e3aa1a61838cfad859
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-04-23 09:48:05 +0000

    commit 1

---
+++ home/src/app/mostre/mostre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostre',
+  standalone: true,
+  imports: [],
+  templateUrl: './mostre.component.html',
+  styleUrl: './mostre.component.css'
+})
+export class MostreComponent {
+
+}
